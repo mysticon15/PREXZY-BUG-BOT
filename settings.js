2035782060ee1a430d284ccff21974836e4f8222
mysticon15 <85706649+mysticon15@users.noreply.github.com>
2024-08-27 18:35:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
-global.ytname = "YT: Prexzyvilla"
-global.socialm = "GitHub: Prexzybooster"
+global.ownernomer = "2347088436547"
+global.ownername = "WULU"
+global.ytname = "YT: WULU"
+global.socialm = "GitHub: WULU"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2347088436547'  //creator number
+global.ownername = 'WULU👑' //owner name
+global.botname = 'WULU ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.packname = 'Sticker By WULU'
+global.author = 'WULU👑\n\nContact: +2347088436547'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -33,7 +33,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
